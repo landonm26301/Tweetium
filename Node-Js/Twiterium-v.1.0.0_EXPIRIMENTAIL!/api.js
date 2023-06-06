8c591b3516f35369e89b2a75f43a585d2211a6ae
Paradox <56404277+landonm26301@users.noreply.github.com>
2023-06-06 15:07:57 -0500

    the v2
    a

--- Node-Js/Twiterium-v.1.0.0_EXPIRIMENTAIL!/api.js
+++ Node-Js/Twiterium-v.1.0.0_EXPIRIMENTAIL!/api.js
@@ -1,78 +1,144 @@
-
 /////////////////////////////////////////////////////////////////////
 // Tweetium-v.1.0.0_EXPIRIMENTAIL! build        by: landon metzger //
 /////////////////////////////////////////////////////////////////////
 
-
-// dependinsets / requierments
-const {Builder, Browser, By, Key, options, until} = require('selenium-webdriver');     // Seliem Webdriver
-const firefox = require('selenium-webdriver/firefox');      // fierfox browser ewb engine
-const delay = require('delay');   // delay for async
-const term = require('terminal-kit').terminal;    // Terminal kit
-
+// dependents / requirements
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver"); // Seliem Webdriver
+const firefox = require("selenium-webdriver/firefox"); // fierfox browser ewb engine
+const delay = require("delay"); // delay for async
+const term = require("terminal-kit").terminal; // Terminal kit
+let options = new firefox.Options();
+const path = require("path");
 
 // Build the driver
-driver = new Builder()
-      .forBrowser(Browser.FIREFOX)
-      .build();
+var driver;
 
-// Global element varieables
+// Global element variables
 var body;
-var IMGButton;
-var buttonRow;
+var Textarea;
+var imgUppload;
 
-
-// MAKE SHURE TO CALL THIS BEFORE YOU DO ANYTHING!!!!
-exports.INIT = async function (auth_token)
-{
+// MAKE SURE TO CALL THIS BEFORE YOU DO ANYTHING!!!!
+exports.INIT = async function (cookiesFile) {
   await term.clear();
-  // Loade the page
-  await term.bgWhite(' * '); term.bgBlue('Loading twitter\n');
-  await driver.get('https://twitter.com/home');
-  await driver.manage().addCookie({ name: 'auth_token', value: auth_token });
-  await driver.get('https://twitter.com/home');
-  await loadElms(); // get elements classes after loading the page
-  await term.clear(); 
-  await term.bgWhite(' * '); term.bgBlue('Success\n');
-}
+  // Lode the page
+  await term.bgWhite(" * ");
+  term.bgBlue("Loading twitter\n");
+  await driver.get("https://twitter.com/home");
+  await addCookies(cookiesFile);
+  await driver.get("https://twitter.com/home");
+  await loadElms("*"); // get elements classes after loading the page
+  await term.clear();
+  await term.bgWhite(" * ");
+  term.bgBlue("Success\n");
+};
+
+////////////// Dinamic messaging System //////////////
+
+// Create new message draft
+// \/ Do this before adding any new text elements or imiges
+exports.NewMsg = async function () {
+  await term("\n");
+  await term.yellow("[New Mesege!] ");
+  term.gray("Adding content:" + "\n");
+  await delay(100);
+  await body.sendKeys("n");
+  await delay(200);
+  await loadElms("*");
+};
+
+// Add text to the curent draft
+// \/ Only do this after a draft has been created!!
+exports.addText = async function (message) {
+  await delay(300);
+  await term.yellow("[Adding!] ");
+  term.gray("Text:" + message + "\n");
+  await delay(100);
+  await body.sendKeys(message);
+};
+
+// Add Imge to the curent draft
+// \/ Only do this after a draft has been created!!
+exports.addImg = async function (root, Filepath) {
+  await delay(300);
+  await loadElms("img");
+  await delay(100);
+  await term.yellow("[Adding!] ");
+  term.gray("Imige:" + Filepath + "\n");
+  await delay(100);
+  await imgUppload.sendKeys(path.join(root, Filepath));
+};
+
+// Send the final draft
+// \/ Make sure that the file has been all the way uploaded before calling
+exports.SendMsg = async function () {
+  await delay(300);
+  await body.sendKeys(Key.CONTROL, Key.ENTER);
+};
+
+//////////////////////////////////////////////////////
+////////////// Genaric mesaging systems //////////////
 
 // Send a post using the web driver
-    // please try to send messeges with a delay. Remenber this bot is actuly typing your messege
-exports.SendMesege = async function (messege)
-{
+// please try to send messages with a delay. Remember this bot is actually typing your message
+exports.SendMessage = async function (message) {
   await term("\n");
-  await term.yellow("[Sending!] "); term.gray(messege + "\n");
+  await term.yellow("[Text!] ");
+  term.gray(message + "\n");
   await delay(500);
-  await body.sendKeys('n');
+  await body.sendKeys("n");
   await delay(300);
-  await body.sendKeys(messege);
+  await body.sendKeys(message);
   await delay(300);
   await body.sendKeys(Key.CONTROL, Key.ENTER);
-}
+};
 
-// Send an imige
-    // path is the file path from our root directory to the imige file.
-    // If you are using an api pass in the url instead
-exports.SendImige = async function (Path)
-{
-  var outpath = 'C:/Users/lando/OneDrive/Documents/1.projects/1. Node/Twiterium/Twiterium-v.1.0.0_EXPIRIMENTAIL!' + Path;
-  await delay(300);
-  await body.sendKeys('n');
-  await delay(500); 
-  await IMGButton.sendKeys(outpath);
-  await delay(500);
+// Send a post with an imige and optonal text
+// This is an experemental feature !!!There might be bugs and this might not work proporly!!!
+exports.SendImige = async function (root, Filepath) {
+  finalpath = path.join(root, Filepath);
+  await term("\n");
+  await term.yellow("[Imige!] ");
+  term.gray(Filepath + "\n");
+  await delay(100);
+  await body.sendKeys("n");
+  await delay(200);
+  await loadElms("img");
+  await delay(100);
+  await imgUppload.sendKeys(finalpath);
+  await delay(2000); // this should long enough to load the imige
   await body.sendKeys(Key.CONTROL, Key.ENTER);
-}
+};
 
-
+//////////////////////////////////////////////////////
 
 // Non interactible functions pleas ignore theas //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// \/ Refresh element selectors.
+async function loadElms(elm) {
+  if (elm == "*") {
+    body = await driver.findElement(By.xpath("(//body)[1]"));
+  } else if (elm == "img") {
+    imgUppload = await driver.findElement(
+      By.xpath(
+        "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/input[1]"
+      )
+    );
+  }
+}
+// \/ Remove Turn on notifacation if it is a thing
 
+// \/ add cookies function
+async function Login() {
+  await driver
+    .manage()
+    .getCookies()
+    .then(function (cookies) {
+      if (!cookies) {
+        DoLogin();
+      }
+    });
+}
 
-  // \/ Load elements in the page and get thair class name and what not | 
-async function loadElms()
-{
-  body = await driver.findElement(By.tagName('body'));
-  buttonRow = await driver.findElement(By.className('r-1s2bzr4'));
-  IMGButton =  await buttonRow.findElement(By.css("[role=button]"));
-} 
+async function DoLogin() {
+  await driver;
+}
--- Node-Js/Twiterium-v.1.0.0_EXPIRIMENTAIL!/test.js
+++ Node-Js/Twiterium-v.1.0.0_EXPIRIMENTAIL!/test.js
@@ -4,7 +4,7 @@ dothedew();
 
 async function dothedew()
 {
-    await twitter.INIT('', '--headless'); // your twiters acount auth token here | find it by going into inspect element and find it in the cookies.
-                                          // change headless to --head to see the browser 
+    await twitter.INIT('', '--headless'); // your twiters acount auth token here | find it by going into inspect element and going into cookies.
+                                          // change headless to --head to see the browser For Debuging.
     await twitter.SendMessage('Hello world!');
 }
